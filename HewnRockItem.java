f18123a372415f97d9fd9c884c23eac0ac4bb1d0
Frank Huang <me@nongraphical.com>
2013-02-27 14:27:40 -0800

    Items

---
+++ HewnRockItem.java
@@ -0,0 +1,5 @@
+public class HewnRockItem extends Item {
+    HewnRockItem(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ Item.java
@@ -0,0 +1,8 @@
+public abstract class Item {
+    int posx, posy;
+
+    public Item(int x, int y) {
+        posx = x;
+        posy = y;
+    }
+}
